41c58d5d066ab1b5655f3f308da2ba9992c108ac
joashcabanilla <joash0199@gmail.com>
2021-03-26 14:32:27 +0800

    first commit

---
+++ page/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Pharmacy Management Process System">
+<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0">
+<link rel="shortcut icon" href="../image/webicon.ico">
+<link rel="stylesheet" href="../style/home.css">
+<title>PMPS</title>
+</head>
+<body>
+    <div class="navbar">
+    <img width="60rem" height="60rem" src="../image/logo.png" />
+    </div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+
+    <div>qwerty</div>
+    <div>qwerty</div>
+
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div><div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div><div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div><div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div>
+    <div>qwerty</div><div>qwerty</div>
+
+    <div>qwerty</div>
+    <div>qwerty</div><div>qwerty</div>
+    <div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div><div>qwerty</div>
+</body>
+</html>
